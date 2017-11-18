84de47375e77beec05d5c028cd4a5661216ef4f8
keita-koga <keita.koga.work@gmail.com>
2017-11-19 02:49:27 +0900

    add #7 migrationファイルを追加

---
+++ database/migrations/2017_11_18_172026_create_accounts_passwords_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsPasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts_passwords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('account_id');
+            $table->string('password_hash', 255);
+            $table->unsignedInteger('lock_version')->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->unique('account_id', 'uq_accounts_passwords_01');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts_passwords');
+    }
+}
